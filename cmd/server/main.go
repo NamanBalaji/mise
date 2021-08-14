de584e00232bc7002fa6d0a60e5f0a89166d0b41
namanQuick <namanb487@gmail.com>
2021-08-14 17:13:13 +0530

    basic server setup and ping pong functionality

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/NamanBalaji/mise/internal/config"
+	"github.com/NamanBalaji/mise/internal/handlers"
+)
+
+func main() {
+
+	log.Println("Starting Mise")
+	app := &config.AppConfig{
+		Version: 1,
+	}
+
+	repo := handlers.NewRepo(app)
+	handlers.NewHandlers(repo)
+
+	port := flag.String("p", "6379", "")
+	portNum := fmt.Sprintf(":%s", *port)
+
+	srv := &http.Server{
+		Addr:    portNum,
+		Handler: routes(),
+	}
+
+	log.Println("Server running on port", portNum)
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ cmd/server/routes.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/NamanBalaji/mise/internal/handlers"
+	"github.com/go-chi/chi"
+)
+
+func routes() http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Get("/ping", handlers.Repo.Ping)
+
+	return mux
+}
